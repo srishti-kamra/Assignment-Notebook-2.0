f952923270b43b9ab0e05fe2b689085d535d72f9
Srishti Kamra <srishtikamra07@gmail.com>
2023-01-31 09:29:18 -0600

    Initial Commit

---
+++ Assignment Notebook 2/Assignment_Notebook_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_Notebook_2App.swift
+//  Assignment Notebook 2
+//
+//  Created by Srishti Kamra  on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_Notebook_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
